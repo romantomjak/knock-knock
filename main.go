a890101c98fa6a93c59f73f088dff3555a00f79c
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2020-02-07 13:59:07 +0000

    add exit codes

--- main.go
+++ main.go
@@ -1,25 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
+	os.Exit(Run(os.Stdin, os.Stdout, os.Stdout, os.Args[1:]))
+}
+
+func Run(stdin io.Reader, stdout, stderr io.Writer, args []string) int {
 	tmpl, err := NewTemplate("/Users/romantomjak/.knock-knock.toml")
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(stderr, err)
+		return 1
 	}
 
 	consul, err := NewConsulClient()
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(stderr, err)
+		return 1
 	}
 
 	vault, err := NewVaultClient()
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(stderr, err)
+		return 1
 	}
 
 	err = tmpl.Execute(consul, vault)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(stderr, err)
+		return 1
 	}
 
-	println(tmpl.Contents())
+	fmt.Fprintln(stdout, tmpl.Contents())
+	return 0
 }
