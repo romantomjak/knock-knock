ab6354b895386010a78a25ff24e82908b56a9405
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2020-02-07 09:23:36 +0000

    parser can execute template

--- template.go
+++ template.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"text/template"
 )
 
 // Template is the internal representation of a configuration file
@@ -18,4 +20,29 @@ func NewTemplate(filename string) (*Template, error) {
 		return nil, fmt.Errorf("template: %s", err)
 	}
 	return &Template{filename, string(contents)}, nil
+}
+
+// Execute evaluates the template
+func (t *Template) Execute() error {
+	tmpl := template.New("")
+
+	tmpl, err := tmpl.Parse(t.contents)
+	if err != nil {
+		return fmt.Errorf("template: %s", err)
+	}
+
+	var out bytes.Buffer
+	err = tmpl.Execute(&out, nil)
+	if err != nil {
+		return fmt.Errorf("template: %s", err)
+	}
+
+	t.contents = out.String()
+
+	return nil
+}
+
+// Contents returns the contents of the template
+func (t *Template) Contents() string {
+	return t.contents
 }
--- template_test.go
+++ template_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -9,5 +11,45 @@ func TestTemplate_TemplateNotFound(t *testing.T) {
 	_, err := NewTemplate("death-star-was-an-inside-job.txt")
 	if err == nil {
 		t.Fatal("expected an error")
+	}
+}
+
+// Test that parser can read and return parsed template
+func TestTemplate_Render(t *testing.T) {
+	in := createTempfile([]byte("hello world"), t)
+	defer deleteTempfile(in, t)
+
+	tmpl, _ := NewTemplate(in.Name())
+
+	err := tmpl.Execute()
+	if err != nil {
+		t.Fatal("unexpected error")
+	}
+
+	out := "hello world"
+	if tmpl.Contents() != out {
+		t.Fatalf("expected %q to match %q", tmpl.Contents(), out)
+	}
+}
+
+func createTempfile(b []byte, t *testing.T) *os.File {
+	f, err := ioutil.TempFile(os.TempDir(), "")
+	if err != nil {
+		t.Errorf("cannot create tempfile: %s", err)
+	}
+
+	if len(b) > 0 {
+		_, err = f.Write(b)
+		if err != nil {
+			t.Errorf("cannot write to tempfile: %s", err)
+		}
+	}
+
+	return f
+}
+
+func deleteTempfile(f *os.File, t *testing.T) {
+	if err := os.Remove(f.Name()); err != nil {
+		t.Errorf("cannot delete tempfile: %s", err)
 	}
 }
